f3652da3cfbcd6725908d631c8b1190a885141bc
cristofer <ml01137152@uls.edu.sv>
2024-09-07 09:01:54 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Mi app</h1>
+  <hr />
+  <MiComponente />
+</template>
+
+<script setup lang="ts">
+import MiComponente from './components/MiComponente.vue'
+</script>
+
+<style scoped></style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 class="h1">Mi primer componente</h1>
+
+    <section>
+      <h3>Número = {{ numero }}</h3>
+    </section>
+
+    <button class="btn" @click="aumentar">+1</button>
+    🥸
+    <button class="btn" @click="disminuir">-1</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+// Definir una variable reactiva 'numero'
+const numero = ref<number>(0)
+
+// Función para aumentar el número
+const aumentar = () => {
+  numero.value++
+}
+
+// Función para disminuir el número
+const disminuir = () => {
+  numero.value--
+}
+</script>
+
+<hr />
